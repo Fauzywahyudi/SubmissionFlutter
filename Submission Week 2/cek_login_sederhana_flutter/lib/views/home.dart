5edab1c20bc95db8af7b2ce987fa6ac486e885cc
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-08-08 17:02:58 +0700

    design home

--- Submission Week 2/cek_login_sederhana_flutter/lib/views/home.dart
+++ Submission Week 2/cek_login_sederhana_flutter/lib/views/home.dart
@@ -1,6 +1,11 @@
 import 'package:cek_login_sederhana_flutter/views/login.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+
 class Home extends StatefulWidget {
   final username;
 
@@ -13,21 +18,111 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => _logout(),
-        label: Text("Logout"),
-        icon: Icon(Icons.exit_to_app),
-      ),
-      body: Container(
-        child: Center(
-          child: Text(
-              "Selamat Datang ${widget.username.toString().toUpperCase()}"),
+      backgroundColor: Colors.blue,
+      // floatingActionButton: _buildFloatingActionButton(),
+      body: SafeArea(
+        child: Container(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height,
+                color: Colors.white,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(70),
+                    bottomRight: Radius.circular(70),
+                  ),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.blue, Colors.lightBlue[200]],
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    _buildAppBar(),
+                    _buildText(),
+                    Expanded(child: Container()),
+                    Container(
+                      height: MediaQuery.of(context).size.width * 0.5,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.blue,
+                            Colors.blueGrey,
+                          ],
+                        ),
+                      ),
+                      child: SvgPicture.asset("assets/images/wellcome.svg"),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Text _buildText() {
+    return Text("Wellcome \"${widget.username.toString().toUpperCase()}\"",
+        style: GoogleFonts.mcLaren(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ));
+  }
+
+  Row _buildAppBar() {
+    return Row(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        Expanded(
+          child: Container(),
+        ),
+        Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.notifications,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  FloatingActionButton _buildFloatingActionButton() {
+    return FloatingActionButton.extended(
+      onPressed: () => _logout(),
+      label: Text("Logout"),
+      icon: Icon(Icons.exit_to_app),
     );
   }
 
