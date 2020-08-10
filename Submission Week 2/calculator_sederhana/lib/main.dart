67c756020b2906cc914fecb201c1c2f813987e9a
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-08-10 15:43:07 +0700

    create project kalkulator sederhana

---
+++ Submission Week 2/calculator_sederhana/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:calculator_sederhana/views/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kalkulator Sederhana Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
