ea41765298a69a34a3e5865d633cf29ae97c18ff
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-08-25 14:26:24 +0700

    ubah response backend @week3-1 #5

--- SubmissionWeek3/form_login_register_mahasiswa_backend/login.php
+++ SubmissionWeek3/form_login_register_mahasiswa_backend/login.php
@@ -1,15 +1,24 @@
 <?php
-include 'koneksi.php';
+require 'koneksi.php';
 
-$pass = $_POST['pass'];
-$nim = $_POST['nim'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $response = array();
+    $nim = $_POST['nim'];
+    $pass = md5($_POST['pass']);
 
-$data = array();
+    $sql = $kon->query("SELECT * FROM mahasiswa WHERE nim_mahasiswa='$nim' AND password='$pass'");
 
-$sql = $kon->query("SELECT * FROM mahasiswa WHERE nim_mahasiswa='$nim' AND password='$pass'");
+    $result = $sql->fetch_array();
 
-while ($fetchData = $sql->fetch_assoc()) {
-    $data[] = $fetchData;
+    if (isset($result)) {
+        $response['value'] = 1;
+        $response['message'] = "Login berhasil";
+        $response['nim'] = $result['nim_mahasiswa'];
+        $response['nama'] = $result['nama_lengkap'];
+        echo json_encode($response);
+    } else {
+        $response['value'] = 0;
+        $response['message'] = "Login gagal";
+        echo json_encode($response);
+    }
 }
-
-echo json_encode($data);
--- SubmissionWeek3/form_login_register_mahasiswa_backend/register.php
+++ SubmissionWeek3/form_login_register_mahasiswa_backend/register.php
@@ -1,11 +1,32 @@
-<?php 
-include 'koneksi.php';
+<?php
+require 'koneksi.php';
 
-$nim = $_POST['nim'];
-$pass = $_POST['pass'];
-$nama = $_POST['nama'];
-$alamat = $_POST['alamat'];
-$jk = $_POST['jk'];
-$jurusan = $_POST['jurusan'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $response = array();
+    $nim = $_POST['nim'];
+    $pass = md5($_POST['pass']);
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $jk = $_POST['jk'];
+    $jurusan = $_POST['jurusan'];
 
-$sql = $kon->query("INSERT INTO `mahasiswa`(`id_mahasiswa`, `nim_mahasiswa`, `password`, `nama_lengkap`, `jenis_kelamin`, `jurusan`, `alamat`) VALUES (NULL,'$nim','$pass','$nama','$jk','$jurusan','$alamat')");
+    $resultCek = $kon->query("SELECT * FROM mahasiswa WHERE nim_mahasiswa='$nim'");
+
+    if (isset($resultCek)) {
+        $response['value'] = 2;
+        $response['message'] = "NIM telah terdaftar";
+        echo json_encode($response);
+    } else {
+        $sql = $kon->query("INSERT INTO `mahasiswa`(`id_mahasiswa`, `nim_mahasiswa`, `password`, `nama_lengkap`, `jenis_kelamin`, `jurusan`, `alamat`) VALUES (NULL,'$nim','$pass','$nama','$jk','$jurusan','$alamat')");
+
+        if ($sql) {
+            $response['value'] = 1;
+            $response['message'] = "Berhasil didaftarkan";
+            echo json_encode($response);
+        } else {
+            $response['value'] = 0;
+            $response['message'] = "Gagal didaftarkan";
+            echo json_encode($response);
+        }
+    }
+}
