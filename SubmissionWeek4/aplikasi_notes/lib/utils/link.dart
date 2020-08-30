5e0f836c7af3f0bcd2c48614de8718249e68d511
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-08-30 14:34:50 +0700

    add splashscreen, assets @week4-2

---
+++ SubmissionWeek4/aplikasi_notes/lib/utils/link.dart
@@ -0,0 +1,6 @@
+class Link {
+  static final String server =
+      "http://192.168.42.224/MentoringFlutter/SubmissionFlutter/SubmissionWeek3/form_login_register_mahasiswa_backend/";
+
+  static final String asset = "assets/images/";
+}
