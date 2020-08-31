80b65c4a8a72f05e050fc55fbcb1c63d3c29a067
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-08-31 12:12:47 +0700

    add proses tambah notes and fix login home page @week4-2

---
+++ SubmissionWeek4/aplikasi_notes_backend/addNotes.php
@@ -0,0 +1,21 @@
+<?php
+require 'koneksi.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $response = array();
+    $id = $_POST['id'];
+    $judul = $_POST['judul'];
+    $isi = $_POST['isi'];
+
+    $sql = $kon->query("INSERT INTO `notes`(`id_notes`, `id_user`, `judul_notes`, `isi_notes`, `tgl_buat`, `tgl_update`) VALUES (NULL,'$id','$judul','$isi',NOW(),NULL)");
+
+    if ($sql) {
+        $response['value'] = 1;
+        $response['message'] = "Notes berhasil dibuat";
+        echo json_encode($response);
+    } else {
+        $response['value'] = 0;
+        $response['message'] = "Notes gagal dibuat";
+        echo json_encode($response);
+    }
+}
